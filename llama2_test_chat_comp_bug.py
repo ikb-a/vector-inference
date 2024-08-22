ebfec5e2214e8b727f528655ed4620e64d725531
ianberlot <ianberlot@v2.cluster.local>
2024-08-22 14:40:44 -0400

    Add minimal scripts demonstrating swapping behaviour. Reduce model context length to demonstrate effect on a smaller GPU

---
+++ llama2_test_chat_comp_bug.py
@@ -0,0 +1,26 @@
+from openai import OpenAI
+
+
+#URL="http://gpu017:8080/v1"
+
+import argparse
+parser = argparse.ArgumentParser()
+parser.add_argument('url')
+args = parser.parse_args()
+URL=args.url
+
+# The url is located in the .vLLM_model-variant_url file in the corresponding model directory.
+client = OpenAI(base_url=URL, api_key="EMPTY")
+
+# Update the model path accordingly
+completion = client.chat.completions.create(
+  model="/model-weights/Llama-2-7b-chat-hf",
+  n=10,
+  #timeout=2,
+  messages=[
+    {"role": "system", "content": "You are yourself."},
+    {"role": "user", "content": "Who are you?"},
+  ]
+)
+
+print(completion)
